3e62458359c7474bc4bc2fedd8fadc3a550b1713
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-06-01 16:19:10 +0200

    Add files via upload

---
+++ Instructions.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Config files
+curl -s https://raw.githubusercontent.com/Emblem-66/gamerblue/refs/heads/main/ConfigFiles.sh | bash
+
+# COPR repo add
+curl -sSL https://raw.githubusercontent.com/emblem-66/gamerblue/refs/heads/main/dnf-repo.list | xargs -r dnf config-manager addrepo
+
+# COPR repo add
+curl -sSL https://raw.githubusercontent.com/emblem-66/gamerblue/refs/heads/main/dnf-copr.list | xargs -r dnf copr enable -y
+
+# DNF remove packages
+curl -sSL https://raw.githubusercontent.com/emblem-66/gamerblue/refs/heads/main/dnf-remove.list | xargs -r dnf remove -y
+
+# DNF install packages
+curl -sSL https://raw.githubusercontent.com/emblem-66/gamerblue/refs/heads/main/dnf-install.list | xargs -r dnf install -y
+
+# DNF install packages
+curl -sSL https://raw.githubusercontent.com/emblem-66/gamerblue/refs/heads/main/systemd.list | xargs -r systemctl enable
+
+# COPR repo remove
+curl -sSL https://raw.githubusercontent.com/emblem-66/gamerblue/refs/heads/main/dnf-copr.list | xargs -r dnf copr remove -y
+
+# Tailscale repo remove
+#rm /etc/yum.repos.d/tailscale.repo
+
+dnf -y install steam
+
+# Check packages
+#curl -sSL https://raw.githubusercontent.com/emblem-66/gamerblue/refs/heads/main/.list | xargs -r rpm -qa | sort | grep
+
+rpm -qa | sort | grep kernel
+rpm -qa | sort | grep freeworld
+rpm -qa | sort | grep mutter
+rpm -qa | sort | grep kwin
+rpm -qa | sort | grep steam
+
